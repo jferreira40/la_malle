306ad7b22e331fde03f46966dca88ba3e16b1ec5
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-05-07 19:35:47 +0200

    Tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      darkBlue: "#0D25B9",
+      blue: "#4A7AE8",
+      red: "#EA6767",
+      pink: "#FD6885",
+      darkGray: "#818181",
+      gray: "#E9E9E9",
+      white: "#FFFFFF",
+      yellow: "#FFD45D",
+      spy: "#6879E2",
+      wolf: "#9162C1",
+      survive: "#E2AE68",
+    },
   },
   variants: {
     extend: {},
