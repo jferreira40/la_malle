bbcefc6fbf44a4be3ba09ae5d6e9620473c51e8c
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-05-11 20:18:23 +0200

    Fix link to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      black: "#000000",
-      darkBlue: "#0D25B9",
-      blue: "#4A7AE8",
-      red: "#EA6767",
-      pink: "#FD6885",
-      darkGray: "#818181",
-      gray: "#E9E9E9",
-      white: "#FFFFFF",
-      yellow: "#FFD45D",
-      spy: "#6879E2",
-      wolf: "#9162C1",
-      survive: "#E2AE68",
-    },
+    extend: {
+      colors: {
+        black: "#000000",
+        darkBlue: "#0D25B9",
+        blue: "#4A7AE8",
+        red: "#EA6767",
+        pink: "#FD6885",
+        darkGray: "#818181",
+        gray: "#E9E9E9",
+        white: "#FFFFFF",
+        yellow: "#FFD45D",
+        spy: "#6879E2",
+        wolf: "#9162C1",
+        survive: "#E2AE68",
+      }
+    }
   },
   variants: {
     extend: {},
