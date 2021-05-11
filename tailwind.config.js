c5166987c3ec505f531cdd586d1fd1c67bd75ebc
Baptiste Sapa <contact@baptistesapa.fr>
2021-05-11 22:35:24 +0200

    Header, intro-bloc, favorites started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -11,12 +13,16 @@ module.exports = {
         pink: "#FD6885",
         darkGray: "#818181",
         gray: "#E9E9E9",
+        lightGray: "#F5F5F5",
         white: "#FFFFFF",
         yellow: "#FFD45D",
         spy: "#6879E2",
         wolf: "#9162C1",
         survive: "#E2AE68",
-      }
+      },
+      fontFamily: {
+        sans: ['Red Hat Text', ...defaultTheme.fontFamily.sans],
+      },
     }
   },
   variants: {
