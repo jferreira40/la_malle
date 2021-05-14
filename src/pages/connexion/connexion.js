3258d9c416db28132f8eb2f3005f6d03f25921fb
Jessica FERREIRA <ferreira.jess40@gmail.com>
2021-05-14 14:13:33 +0200

    Change image and wip inscription js

--- src/pages/connexion/connexion.js
+++ src/pages/connexion/connexion.js
@@ -1,3 +1,9 @@
 function displayInscription() {
   window.location = 'inscription.html';
 }
+
+function inscription(){
+  const form = document.getElementById("inscription");
+  const formData = new FormData(form);
+  console.log(formData);
+}
