2812c8eb75d29633803d63edec583e9d55f67940
JessicaRhaegal <jessica@rhaegal.com>
2021-05-16 15:19:50 +0200

    Fix la malle title and wip promise

--- front/src/pages/connexion/connexion.js
+++ front/src/pages/connexion/connexion.js
@@ -1,28 +1,34 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  document.getElementById('inscription').onsubmit = () => {
-    inscription(event);
-  }
+  document.getElementById('inscription').onsubmit = event => onSubmitInscription(event)
 });
 
 function displayInscription() {
   window.location = 'inscription.html';
 }
 
-function inscription(event) {
+function onSubmitInscription(event) {
   event.preventDefault();
-  const mdp = document.getElementById('mdp').value;
-  const repeat = document.getElementById('repeatemdp').value;
 
-  console.log(mdp);
-  console.log(repeat)
-  if(mdp === repeat) {
-    const form = document.getElementById("inscription");
-    const formData = new FormData(form);
+  const password = document.getElementById("mdp").value;
+  const repeatPassword = document.getElementById("repeatemdp").value;
 
-    console.log(formData);
-    /*fetch("http://34.105.145.42/api/register.php", {
-      method: "POST",
-      body: formData
-    })*/
+  if (Object.is(password, repeatPassword)) {
+    const formData = new FormData(event.target);
+
+    return new Promise(() => {
+      fetch("http://34.105.145.42/api/register.php", {
+        method: "POST",
+        headers: {
+          "Authorization" : "Bearer <JWT>"
+        },
+        body: formData
+      })
+          .then(res => {
+            if (res.ok)
+              console.log(res)
+            else
+              console.log(res.text());
+          })
+    })
   }
 }
